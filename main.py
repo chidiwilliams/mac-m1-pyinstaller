27df25b9d7a4e1697f9f30192ee98c052f2e5f43
Chidi Williams <williamschidi1@gmail.com>
2023-01-28 15:59:02 +0000

    Update

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import logging
+import subprocess
+
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
+    cmd = subprocess.run(['ffmpeg', '-h'])
+    logging.warning('subprocess exited with code %s', cmd.returncode)
     print_hi('PyCharm')
